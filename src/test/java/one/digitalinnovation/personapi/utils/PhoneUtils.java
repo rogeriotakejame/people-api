5b8ac6bdee570a4ba41e284fd6a1edc3ce939392
rogeriotakejame <rogerio.takejame@gmail.com>
2021-08-11 11:56:45 -0300

    Create unit tests for create Person

---
+++ src/test/java/one/digitalinnovation/personapi/utils/PhoneUtils.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personapi.utils;
+
+import one.digitalinnovation.personapi.dto.request.PhoneDTO;
+import one.digitalinnovation.personapi.entity.Phone;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import java.time.LocalDate;
+
+public class PhoneUtils {
+    private static final String PHONE_NUMBER = "(00)00000-0000";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    public static final long PHONE_ID = 2L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
